f9224d8c5c44f8d7565db34d8ca1d95391cc9f02
rcrespo9 <rudy.crespo20@gmail.com>
2018-10-29 16:14:30 -0400

    starting on chart

--- src/components/WeightTrackerChart.vue
+++ src/components/WeightTrackerChart.vue
@@ -1,15 +1,35 @@
 <template>
   <card header="Weight Tracker Chart">
+    <canvas id="WeightTrackerChart"></canvas>
   </card>
 </template>
 
 <script>
+import Chart from 'chart.js'
 import Card from './AppCard'
 
 export default {
   name: 'WeightTrackerChart',
   components: {
     Card
+  },
+  created() {
+    this.$store.dispatch('getEntries')
+  },
+  mounted() {
+    const $ctx = document.getElementById('WeightTrackerChart')
+    const lineChart = new Chart($ctx, {
+      type: 'line',
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }      
+    })
   }
 }
 </script>
